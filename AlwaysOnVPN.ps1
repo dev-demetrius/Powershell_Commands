8e640418a219fe9b98b9fe985b629e4d263bba46
dev-demetrius <46727509+dev-demetrius@users.noreply.github.com>
2023-03-03 09:47:09 -0500

    Update AlwaysOnVPN.ps1

--- AlwaysOnVPN.ps1
+++ AlwaysOnVPN.ps1
@@ -1,9 +1,11 @@
+# Loop to check connection status every 5 minutes
 while ($true)
 {
     $vpnname = "VPN_Server"
     $vpn = Get-VpnConnection -AllUserConnection | where {$_.Name -eq $vpnname}
     if ($vpn.ConnectionStatus -eq "Disconnected")
     {
+        # Referenced .bat file that includes connection and credentials
         start-process "C:\Windows\AutoConnectVPN.bat"
     }
 
