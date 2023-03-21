6c92da8a1df6bd86dc7880323c9e3909a0747f8c
dev-demetrius <46727509+dev-demetrius@users.noreply.github.com>
2023-03-21 09:36:20 -0400

    Update CreateVPNConnection.ps1

--- CreateVPNConnection.ps1
+++ CreateVPNConnection.ps1
@@ -1,3 +1,6 @@
+# This script sets the execution policy to bypass to allow it to run without restrictions
 Set-ExecutionPolicy Bypass -force
 
+# This command adds a VPN connection named "VPN_Server" with the specified settings
 Add-VpnConnection -Name "VPN_Server" -ServerAddress "192.168.1.1" -AllUserConnection $true -SplitTunneling $true -AuthenticationMethod MSChapv2 -TunnelType "L2tp" -EncryptionLevel Required -PassThru
+
