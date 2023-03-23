b74b11f48152a8a2265a9a707f78c47307a9cbfc
dev-demetrius <46727509+dev-demetrius@users.noreply.github.com>
2023-03-23 08:33:49 -0400

    Update install.ps1

--- File Scripts/install.ps1
+++ File Scripts/install.ps1
@@ -30,23 +30,3 @@ Expand-Archive -Path "D:\dsa_station.zip" -DestinationPath "C:\"
 Add-MpPreference -ExclusionPath "C:\path\to\your\folder"
 
 
-$accountSid = "AC182cc57e35402f1eeb4f842f980ac96c"
-$authToken = "7ff3e98603b58c3e3ff82a2bd376e1b0"
-
-# Set your Twilio phone number and recipient phone number
-$twilioPhoneNumber = "+18444390071"
-$recipientPhoneNumber = "+12294499921"
-
-# Send the message using the Twilio API
-$uri = "https://api.twilio.com/2010-04-01/Accounts/$accountSid/Messages"
-$body = @{
-    From = $twilioPhoneNumber
-    To = $recipientPhoneNumber
-    Body = $message
-} | ConvertTo-Json
-$headers = @{
-    Authorization = "Basic " + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes("${accountSid}:${authToken}"))
-    "Content-Type" = "application/json"
-}
-
-Invoke-RestMethod -Uri $uri -Method POST -Body $body -Headers $headers
